68324c51934553c6a286c0ee9ca2c684a01b9606
Sertaç Kocagil <srtc.kcgl@gmail.com>
2023-02-23 17:48:09 +0300

    hatali cozum düzeltildi. fonksiyon artik tek arguman aliyor.

--- ip/index.js
+++ ip/index.js
@@ -73,89 +73,80 @@ async function ipAdresimiAl() {
 //kodlar buraya gelecek
 
 
-// {
-// 	"sorgu":"46.196.81.16",
-// 	"durum":"OK",
-// 	"kıta":"Asia",
-// 	"ülke":"Turkey",
-// 	"ülkeKodu":"TR",
-// 	"ülkebayrağı":"https:\/\/apis.ergineer.com\/ulkebayraklari\/TR",
-// 	"bölge":"34",
-// 	"bölgeAdı":"Istanbul",
-// 	"şehir":"Istanbul",
-// 	"zip":"34096",
-// 	"enlem":41.014499999999998181010596454143524169921875,
-// 	"boylam":28.9532999999999987039700499735772609710693359375,
-// 	"saatdilimi":"Europe\/Istanbul",
-// 	"parabirimi":"TRY",
-// 	"isp":"Turksat Internet Services",
-// 	"organizasyon":"",
-// 	"as":"AS47524 Turksat Uydu Haberlesme ve Kablo TV Isletme A.S."
-//   }
-// <div class="card">
-// <img src={ülke bayrağı url} />
-// <div class="card-info">
-// 	<h3 class="ip">{ip adresi}</h3>
-// 	<p class="ulke">{ülke bilgisi (ülke kodu)}</p>
-// 	<p>Enlem: {enlem} Boylam: {boylam}</p>
-// 	<p>Şehir: {şehir}</p>
-// 	<p>Saat dilimi: {saat dilimi}</p>
-// 	<p>Para birimi: {para birimi}</p>
-// 	<p>ISP: {isp}</p>
-
-const cardYapici = (ip, bayrakUrl, ulke, ulkeKodu, enlem, boylam, sehir, saat, para, isp) => {
+const cardYapici = (data) => {
 	const card = document.createElement("div");
 	card.classList.add("card");
-
+  
 	const bayrak = document.createElement("img");
-	bayrak.setAttribute("src", bayrakUrl)
+	bayrak.setAttribute("src", data?.["ülkebayrağı"]);
 	card.append(bayrak);
-
+  
 	const cardInfo = document.createElement("div");
 	cardInfo.classList.add("card-info");
 	card.append(cardInfo);
-
+  
 	const baslik = document.createElement("h3");
 	baslik.classList.add("ip");
-	baslik.textContent = ip;
+	baslik.textContent = `IP: ${data?.sorgu}`;
 	cardInfo.append(baslik);
-
+  
 	const ulkeBilgisi = document.createElement("p");
 	ulkeBilgisi.classList.add("ulke");
-	ulkeBilgisi.textContent = `${ulke} (${ulkeKodu})`;
+	ulkeBilgisi.textContent = `${data?.["ülke"]} (${data?.["ülkeKodu"]})`;
 	cardInfo.append(ulkeBilgisi);
-
+  
 	const enBoy = document.createElement("p");
-	enBoy.textContent=`Enlem: ${enlem} Boylam: ${boylam}`;
+	enBoy.textContent = `Enlem: ${data?.enlem} Boylam: ${data?.boylam}`;
 	cardInfo.append(enBoy);
-
+  
 	const sehirBilgi = document.createElement("p");
-	sehirBilgi.textContent=`Şehir: ${sehir}`;
+	sehirBilgi.textContent = `Şehir: ${data?.["şehir"]}`;
 	cardInfo.append(sehirBilgi);
-
-
+  
 	const saatDilimi = document.createElement("p");
-	saatDilimi.textContent=`Saat dilimi: ${saat}`;
+	saatDilimi.textContent = `Saat dilimi: ${data?.saatdilimi}`;
 	cardInfo.append(saatDilimi);
-
+  
 	const paraBirimi = document.createElement("p");
-	paraBirimi.textContent=`Para birimi: ${para}`;
+	paraBirimi.textContent = `Para birimi: ${data?.parabirimi}`;
 	cardInfo.append(paraBirimi);
-
+  
 	const ispBilgi = document.createElement("p");
-	ispBilgi.textContent=`ISP: ${isp}`;
+	ispBilgi.textContent = `ISP: ${data?.isp}`;
 	cardInfo.append(ispBilgi);
-
+  
 	return card;
-}
+  };
+
+const cardListele = () => {
+	const cardContainer = document.querySelector(".cards");
+	axios 
+	.get("https://apis.ergineer.com/ipadresim")
+	.then ((res)=>{
+		 const ipAdresi = res.data ;
+		 console.log(ipAdresi);
+		axios
+			.get(`https://apis.ergineer.com/ipgeoapi/${ipAdresi}`)
+			.then((res) => {
+				console.log(res);
+				const cardBilgi = cardYapici(res.data);
+				cardContainer.append(cardBilgi);
+			})
+	})
+	.catch(err=>{
+		console.log("error",err);
+	});
+	}
+cardListele();
+
 
 // ipAdresimiAl();
 
 //  !!!!  yukarıdaki fonksiyonu iceride calıştıramadım. içiçe 2 axios .get olarak kendim yazdım.    !!!!
 // console.log(benimIP);
 
-const cardListele = () => {
-	const cardContainer = document.querySelector(".cards");
+// const cardListele = () => {
+// 	const cardContainer = document.querySelector(".cards");
 
 	// let myIp = "https://apis.ergineer.com/ipgeoapi/" + benimIP; 
 
@@ -165,34 +156,123 @@ const cardListele = () => {
 
 			// calısmadı
 
-	axios 
-	.get("https://apis.ergineer.com/ipadresim")
-	.then ((res)=>{
-		 const ipAdresi = res.data ;
-		 console.log(ipAdresi);
-		axios
-			.get(`https://apis.ergineer.com/ipgeoapi/${ipAdresi}`)
-			.then((res) => {
-				const cardBilgi = cardYapici(
-					res.data["sorgu"],
-					res.data["ülkebayrağı"],
-					res.data["ülke"],
-					res.data["ülkeKodu"],
-					res.data["enlem"],
-					res.data["boylam"],
-					res.data["şehir"],
-					res.data["saatdilimi"],
-					res.data["parabirimi"],
-					res.data["isp"]);
-
-				cardContainer.append(cardBilgi);
-			})
-			.catch(err=>{
-				console.log("error",err);
-			})
-		})
-}
-cardListele();
+// ESKİ ÇÖZÜM (birden fazla parametre alarak)
 
 
+// const cardYapici = (ip, bayrakUrl, ulke, ulkeKodu, enlem, boylam, sehir, saat, para, isp) => {
+// 	const card = document.createElement("div");
+// 	card.classList.add("card");
 
+// 	const bayrak = document.createElement("img");
+// 	bayrak.setAttribute("src", bayrakUrl)
+// 	card.append(bayrak);
+
+// 	const cardInfo = document.createElement("div");
+// 	cardInfo.classList.add("card-info");
+// 	card.append(cardInfo);
+
+// 	const baslik = document.createElement("h3");
+// 	baslik.classList.add("ip");
+// 	baslik.textContent = ip;
+// 	cardInfo.append(baslik);
+
+// 	const ulkeBilgisi = document.createElement("p");
+// 	ulkeBilgisi.classList.add("ulke");
+// 	ulkeBilgisi.textContent = `${ulke} (${ulkeKodu})`;
+// 	cardInfo.append(ulkeBilgisi);
+
+// 	const enBoy = document.createElement("p");
+// 	enBoy.textContent=`Enlem: ${enlem} Boylam: ${boylam}`;
+// 	cardInfo.append(enBoy);
+
+// 	const sehirBilgi = document.createElement("p");
+// 	sehirBilgi.textContent=`Şehir: ${sehir}`;
+// 	cardInfo.append(sehirBilgi);
+
+
+// 	const saatDilimi = document.createElement("p");
+// 	saatDilimi.textContent=`Saat dilimi: ${saat}`;
+// 	cardInfo.append(saatDilimi);
+
+// 	const paraBirimi = document.createElement("p");
+// 	paraBirimi.textContent=`Para birimi: ${para}`;
+// 	cardInfo.append(paraBirimi);
+
+// 	const ispBilgi = document.createElement("p");
+// 	ispBilgi.textContent=`ISP: ${isp}`;
+// 	cardInfo.append(ispBilgi);
+
+// 	return card;
+// }
+
+
+// 	axios 
+// 	.get("https://apis.ergineer.com/ipadresim")
+// 	.then ((res)=>{
+// 		 const ipAdresi = res.data ;
+// 		 console.log(ipAdresi);
+// 		axios
+// 			.get(`https://apis.ergineer.com/ipgeoapi/${ipAdresi}`)
+// 			.then((res) => {
+// 				console.log(res);
+// 				const cardBilgi = cardYapici(
+// 					res.data["sorgu"],
+// 					res.data["ülkebayrağı"],
+// 					res.data["ülke"],
+// 					res.data["ülkeKodu"],
+// 					res.data["enlem"],
+// 					res.data["boylam"],
+// 					res.data["şehir"],
+// 					res.data["saatdilimi"],
+// 					res.data["parabirimi"],
+// 					res.data["isp"]);
+// 				cardContainer.append(cardBilgi);
+// 			})
+// 			.catch(err=>{
+// 				console.log("error",err);
+// 			})
+// 		})
+// }
+// cardListele();
+
+
+
+
+
+
+// const cardContainer = document.querySelector(".cards");
+
+// let myIp = null;
+// async function getMyIp() {
+//   await axios
+//     .get("https://apis.ergineer.com/ipadresim")
+//     .then(function (response) {
+//       // handle success
+//       myIp = response.data;
+//       return myIp;
+//     })
+//     .catch(function (error) {
+//       // handle error
+//       console.log(error);
+//     })
+//     .finally(function () {
+//       // always executed
+//       console.log("myIp", myIp);
+//     });
+// }
+
+// async function getApiDetails() {
+//   await getMyIp();
+
+//   axios
+//     .get(`https://apis.ergineer.com/ipgeoapi/${myIp}`)
+//     .then(function (response) {
+//       cardContainer.append(cardYapici(response.data));
+//     })
+//     .catch(function (error) {
+//       // handle error
+//       console.log(error);
+//     });
+// }
+
+// getApiDetails();
